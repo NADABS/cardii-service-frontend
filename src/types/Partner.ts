8860aa557e11dec63fbab250aad72a15b6f43c4b
ObedEffum <160004995+ObedEffum@users.noreply.github.com>
2025-10-16 01:39:43 +0000

    Created partner type and partners table

---
+++ src/types/Partner.ts
@@ -0,0 +1,12 @@
+export default interface Partner {
+    externalId: string;
+    internalId: string;
+    name: string;
+    email: string;
+    phone: string;
+    status: string;
+    preferredChannel: string;
+    deviceType: string;
+    location: string;
+    createdAt: string;
+}
