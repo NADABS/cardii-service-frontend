be7cba853a1602f898827e674098c70b526b1b83
Jonadab Kwamlah <jkwamlah@outlook.com>
2025-10-22 21:41:34 +0000

    Refactored dashboard

--- src/app/(dashboard)/overview/page.tsx
+++ src/app/(dashboard)/overview/page.tsx
@@ -2,16 +2,26 @@
 import StatisticsSection from "@/src/components/dashboard/StatisticsSection";
 import {PerformanceComponent} from "@/src/components/dashboard/PerformanceComponent";
 import {CurrentTasksTable} from "@/src/components/dashboard/CurrentTasksTable";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {FiUser} from "react-icons/fi";
 import {MdVerified} from "react-icons/md";
 import {GoUnverified} from "react-icons/go";
 
 export default function OverviewPage() {
+    const [user, setUser] = useState<{ name: string } | null>(null);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("userDetails");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
 
     return (
         <div className="w-full h-full overflow-hidden">
-            <h1 className="text-2xl font-bold">Hello, John</h1>
+            <h1 className="text-2xl font-bold">
+                Hello, {user?.name ? user.name.split(" ")[0] : "Guest"}
+            </h1>
             <div className="text-gray-400">
                 Track your partner registrations here
             </div>
@@ -19,24 +29,24 @@ export default function OverviewPage() {
                 <StatisticsSection stats={[
                     {
                         icon: FiUser,
-                        title: "Registered Partners",
+                        title: "Partners",
                         value: 18,
                         change: "+8 partners",
                     },
                     {
                         icon: MdVerified,
-                        title: "Verified Partners",
+                        title: "Users",
                         value: "9",
                         change: "-2 partners",
                     },
                     {
                         icon: GoUnverified,
-                        title: "Unverified Partners",
+                        title: "Campaigns",
                         value: "9",
                         change: "+2 partners",
                     },
                 ]}/>
-                <PerformanceComponent/>
+                {/*<PerformanceComponent/>*/}
                 <CurrentTasksTable/>
             </div>
         </div>
