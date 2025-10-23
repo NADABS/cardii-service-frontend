c70b975821deaeaf0288d9d1b251fd50a03d1566
ObedEffum <160004995+ObedEffum@users.noreply.github.com>
2025-10-23 16:05:05 +0000

    Added get request to overview page

--- src/app/(dashboard)/overview/page.tsx
+++ src/app/(dashboard)/overview/page.tsx
@@ -2,16 +2,43 @@
 import StatisticsSection from "@/src/components/dashboard/StatisticsSection";
 import {PerformanceComponent} from "@/src/components/dashboard/PerformanceComponent";
 import {CurrentTasksTable} from "@/src/components/dashboard/CurrentTasksTable";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {FiUser} from "react-icons/fi";
 import {MdVerified} from "react-icons/md";
 import {GoUnverified} from "react-icons/go";
+import {getItem} from "@/src/lib/storage";
+import useFetch from "@/src/hooks/useFetch";
+import {handleError} from "@/src/lib/errorHandler";
 
 export default function OverviewPage() {
 
+    const [userDetails, setUserDetails] = useState({
+        name: "",
+        externalId: "",
+        bearerToken: "",
+    })
+
+    const {data, isLoading, error} = useFetch(`${process.env.NEXT_PUBLIC_CARDII_API_BASE_URL}/v1/admins/dashboard`, ["dashboard"], {}, userDetails.bearerToken, userDetails.bearerToken!=="")
+
+    useEffect(() => {
+        setUserDetails(getItem("userDetails"))
+    }, []);
+
+    useEffect(() => {
+        if(data) {
+            console.log("data", data.data);
+        }
+    }, []);
+
+    useEffect(() => {
+        if(error) {
+            handleError(error);
+        }
+    }, [error]);
+
     return (
         <div className="w-full h-full overflow-hidden">
-            <h1 className="text-2xl font-bold">Hello, John</h1>
+            <h1 className="text-2xl font-bold">Hello, {userDetails.name}</h1>
             <div className="text-gray-400">
                 Track your partner registrations here
             </div>
